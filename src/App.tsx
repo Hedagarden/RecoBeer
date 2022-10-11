12fd398325f0529659279195d7df3829e126b6c8
Richard Sanborn <richard.sanborn@recopart.se>
2022-10-11 08:15:34 +0200

    adds mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import React, { useState, useEffect } from 'react';
 import logo from './recobeerlogo_v3.png';
 import './App.css';
 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 type Participant = {
   id: number;
   name: string;
@@ -9,6 +24,13 @@ type Participant = {
 };
 
 function App() {
+  let theme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  theme = responsiveFontSizes(theme);
+
   const [participantCounter, setParticipantCounter] = useState<number>(0);
   const [participants, setParticipants] = useState<Participant[]>([]);
   const [newParticipantName, setNewParticipantName] = useState<string>('');
@@ -45,6 +67,10 @@ function App() {
     return current;
   }
 
+  const handleResetParticipants = () => {
+    setParticipants([]);
+  };
+
   const handleUpdatePoints = (id: number, points: string) => {
     if (parseInt(points) > 10) {
       points = '10';
@@ -61,6 +87,15 @@ function App() {
     );
   };
 
+  const handleRemoveParticipant = (id: number) => {
+    var array = [...participants];
+    var index = array.findIndex((e) => e.id === id);
+    if (index !== -1) {
+      array.splice(index, 1);
+      setParticipants(array);
+    }
+  };
+
   const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       addParticipant(newParticipantName);
@@ -68,40 +103,93 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>recobeer</p>
-      </header>
-      <div>
-        <p>avg points</p>
-        {participants &&
-          participants.map((e) => (
-            <p key={e.id}>
-              {e.name} :
-              <input
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="lg">
+        <CssBaseline />
+
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{ marginTop: '10px' }}
+          />
+          <Typography variant="h2" gutterBottom>
+            recobeer
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={12}>
+              {participants &&
+                participants.map((e) => (
+                  <Grid key={e.id} container spacing={2}>
+                    <Grid item xs={12} md={4}>
+                      {e.name}
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                      <TextField
+                        size="small"
+                        type="text"
+                        onChange={(evt) =>
+                          handleUpdatePoints(e.id, evt.target.value)
+                        }
+                        value={e.points}
+                      />
+                    </Grid>
+
+                    <Grid item xs={12} md={4}>
+                      <Button
+                        startIcon={<DeleteIcon />}
+                        variant="contained"
+                        id="removeParticipant"
+                        onClick={() => handleRemoveParticipant(e.id)}>
+                        remove
+                      </Button>
+                    </Grid>
+                  </Grid>
+                ))}
+
+              {participants.length === 0 && <p>no participants yet..</p>}
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <TextField
+                size="small"
+                placeholder="name"
                 type="text"
-                onChange={(evt) => handleUpdatePoints(e.id, evt.target.value)}
-                value={e.points}
+                onChange={(evt) => setNewParticipantName(evt.target.value)}
+                value={newParticipantName}
+                onKeyDown={handleEnter}
               />
-            </p>
-          ))}
-        {participants.length === 0 && <p>no participants yet..</p>}
-        <input
-          type="text"
-          onChange={(evt) => setNewParticipantName(evt.target.value)}
-          value={newParticipantName}
-          onKeyDown={handleEnter}
-        />
-        <button
-          id="addParticipant"
-          onClick={() => addParticipant(newParticipantName)}>
-          Add participant
-        </button>
-        <p>total/average: </p>
-        {totalScore} / {averageScore.toFixed(2)}
-      </div>
-    </div>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Button
+                variant="contained"
+                id="addParticipant"
+                onClick={() => addParticipant(newParticipantName)}>
+                add participant
+              </Button>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Button
+                variant="contained"
+                id="resetParticipants"
+                onClick={() => handleResetParticipants()}>
+                reset
+              </Button>
+            </Grid>
+          </Grid>
+          <p>total/average: </p>
+          {totalScore} / {averageScore.toFixed(2)}
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
